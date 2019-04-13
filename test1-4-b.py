7e1e386a993e1b84d224645ecf5ed627b79e7a25
ei1704 <ryopey1231@ezweb.ne.jp>
2019-04-13 17:54:17 +0900

    memo 1-4-b

--- test1-4-b.py
+++ test1-4-b.py
@@ -1,12 +1,20 @@
 from gpiozero import PWMLED
 from time import sleep
+from gpiozero import Button
+
+button = Button(24,pull_up=False)
 
 led1 = PWMLED(23)
 
 led1.value = 0
 
 while True:
+
+    if button.is_pressed:
+        break
     led1.value += 0.2
     sleep(0.5)
     if led1.value==1:
         led1.value=0
+    
+
